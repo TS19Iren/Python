7924427eea04eda94c035c61b9ef7b25f72d81de
«Iren» <guzlik_iris@mail.ru>
2022-12-16 10:55:19 +0300

    task6

--- task6.py
+++ task6.py
@@ -5,5 +5,15 @@
 # 5 -> -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
 number = int(input('Введите число: '))
 
-for i in range(-number,number+1,1):
-    print(i, end=' ')
+# for i in range(-number,number+1,1):
+#     print(i, end=' ')
+
+# my_list = []
+# for i in range (-number,number+1):
+#     my_list.append(i)
+# print(*my_list, sep=', ')
+
+my_list = []
+for i in range (-number,number+abs(number)//number , abs(number)//number):
+    my_list.append(i)
+print(*my_list, sep=', ')
