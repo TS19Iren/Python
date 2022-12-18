1ec0aadfc5e5eea45ba9baee0b1ad61e3928e8bc
«Iren» <guzlik_iris@mail.ru>
2022-12-18 12:34:12 +0300

    task9

---
+++ task9.py
@@ -0,0 +1,20 @@
+# Задайте список из n чисел последовательности (1 + 1/n)^n,
+# выведеите его на экран, а так же сумму элементов списка.
+# Пример:
+# Для n=4 -> [2, 2.25, 2.37, 2.44]
+# Сумма 9.06
+from decimal import Decimal
+
+number = int(input('Введите число: '))
+my_list = []
+new_number = 0
+summ=0
+if number == 0:
+    print('Введите число больше 0!')
+else:
+    for n in range(1, number+1):
+        new_number = round((1 + (1 / n)) ** n, 2)
+        my_list.append(new_number)
+        summ=summ+new_number
+    print(f'Для number = {number} -> {my_list}')
+    print(f'Сумма элементов равна: {summ}')
